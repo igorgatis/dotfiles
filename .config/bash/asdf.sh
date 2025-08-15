314066a0a6f648cee188a79e3a07278d9caf622c
Igor Gatis <igorgatis@MacBook-Pro-de-UME.local>
2025-08-15 17:53:17 -0300

    Fixed brew and asdf

--- .config/bash/asdf.sh
+++ .config/bash/asdf.sh
@@ -1,6 +1,6 @@
 if command -v asdf &> /dev/null; then
-  asdf set python system
-  asdf set nodejs system
+  export ASDF_PYTHON_VERSION=system
+  export ASDF_NODEJS_VERSION=system
   export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
   . <(asdf completion bash)
 fi
--- .config/bash/brew.sh
+++ .config/bash/brew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BREW=$(which /opt/homebrew/bin || which /home/linuxbrew/.linuxbrew/bin/brew)
+BREW=$(which /opt/homebrew/bin/brew || which /home/linuxbrew/.linuxbrew/bin/brew)
 if [ -n "$BREW" ]; then
   eval "$($BREW shellenv)"
   export HOMEBREW_AUTO_UPDATE_SECS=2592000
