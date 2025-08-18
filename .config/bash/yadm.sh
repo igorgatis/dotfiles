2468533ad2cedcf27d018f9a3e20cdd49b8070be
Igor Gatis <igorgatis@gmail.com>
2025-08-18 11:01:38 -0300

    wip

--- .config/bash/yadm.sh
+++ .config/bash/yadm.sh
@@ -2,18 +2,29 @@
 #!/bin/bash
 
 if command -v yadm >/dev/null 2>&1; then
-  # Check once per day
+  local update_file="$HOME/.config/bash/.yadm_changes"
+  local ts_file="$HOME/.config/bash/.yadm_last_check"
+
+  function check_yadm_async() {
+    if [[ -n "$(yadm status --porcelain)" ]]; then
+      echo "yadm: pending local changes." > "$update_file"
+    else
+      yadm fetch >/dev/null 2>&1
+      if ! yadm diff --quiet HEAD origin/main; then
+        echo "yadm: update available." > "$update_file"
+      else
+        rm -f "$update_file"
+      fi
+    fi
+  }
+
   local ts=$(date +%Y-%m-%d)
-  local file="$HOME/.config/bash/.yadm_last_check"
-  if [[ "$(cat "$file" 2>/dev/null || echo "")" != "$ts" ]]; then
-    echo "$ts" > "$file"
-    yadm fetch >/dev/null 2>&1 & disown
-    local local_commit=$(yadm rev-parse @ 2>/dev/null)
-    local remote_commit=$(yadm rev-parse @{upstream} 2>/dev/null)
-    if [[ "$local_commit" != "$remote_commit" ]]; then
-      echo "yadm: update available."
-    elif [[ -n "$(yadm status --porcelain)" ]]; then
-      echo "yadm: pending local changes."
+  if [[ "$(cat "$ts_file" 2>/dev/null || echo "")" != "$ts" ]]; then
+    echo "$ts" > "$ts_file"
+    if [[ -f "$update_file" ]]; then
+      cat "$update_file"
+    else
+      check_yadm_async & disown
     fi
   fi
 fi
