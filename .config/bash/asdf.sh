d176f3786cd80092b62593d1efc279bbd2a3d9bc
Igor Gatis <igorgatis@MacBook-Pro-de-UME.local>
2025-08-31 16:40:57 -0300

    Added ASDF_GOLANG_VERSION

--- .config/bash/asdf.sh
+++ .config/bash/asdf.sh
@@ -1,6 +1,7 @@
 if command -v asdf &> /dev/null; then
   export ASDF_PYTHON_VERSION=system
   export ASDF_NODEJS_VERSION=system
+  export ASDF_GOLANG_VERSION=system
   export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
   . <(asdf completion bash)
 fi
