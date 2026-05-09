190f8bc198b4a98be129ebad61f5307fcb8c654a
Igor Gatis <igorgatis@gmail.com>
2026-05-09 13:17:35 -0300

    wip

--- .config/sh/aliases.sh
+++ .config/sh/aliases.sh
@@ -2,7 +2,6 @@
 # aliases.sh - Shell-agnostic aliases
 
 alias ..='cd ..'
-alias cc='claude'
 alias du='du -h'
 alias grep='grep --color=auto'
 alias ls='ls -h --color=auto'
@@ -11,10 +10,6 @@ alias vi='vim'
 
 if [ -n "$__ON_TERMUX" ] && command -v proot-distro >/dev/null 2>&1; then
   debian() {
-    if [ "$#" -eq 0 ]; then
-      proot-distro login debian --user igorgatis
-    else
-      proot-distro login debian --user igorgatis -- bash -lc "$*"
-    fi
+    proot-distro login debian --user igorgatis --isolated
   }
 fi
